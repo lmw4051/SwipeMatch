8fea4c77930514e2672ffa3a6ef13e21d8ae0f57
David <dvdlee41@gmail.com>
2020-12-19 20:41:52 +0800

    . Adding ViewModel for MVVM Architecture

---
+++ SwipeMatch/Models/User.swift
@@ -0,0 +1,25 @@
+//
+//  User.swift
+//  SwipeMatch
+//
+//  Created by David on 2020/12/19.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import UIKit
+
+struct User {
+  let name: String
+  let age: Int
+  let profession: String
+  let imageName: String
+  
+  func toCardViewModel() -> CardViewModel {
+    let attributedText = NSMutableAttributedString(string: name, attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy)])
+    attributedText.append(NSAttributedString(string: " \(age)", attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .regular)]))
+    
+    attributedText.append(NSAttributedString(string: "\n\(profession)", attributes: [.font: UIFont.systemFont(ofSize: 20, weight: .regular)]))
+        
+    return CardViewModel(imageName: imageName, attributedString: attributedText, textAlignment: .left)
+  }
+}
---
+++ SwipeMatch/ViewModels/CardViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  CardViewModel.swift
+//  SwipeMatch
+//
+//  Created by David on 2020/12/19.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import UIKit
+
+struct CardViewModel {
+  // Define the properties that are view will display/render out
+  let imageName: String
+  let attributedString: NSAttributedString
+  let textAlignment: NSTextAlignment
+}
