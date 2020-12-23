52e64ee835dee4276283f734e1090238a2f57304
David <dvdlee41@gmail.com>
2020-12-23 13:38:11 +0800

    . Adding saving user photo function

--- SwipeMatch/Controllers/SettingsController.swift
+++ SwipeMatch/Controllers/SettingsController.swift
@@ -99,10 +99,22 @@ class SettingsController: UITableViewController {
   }
   
   fileprivate func loadUserPhotos() {
-    guard let imageUrl = user?.imageUrl1,
-      let url = URL(string: imageUrl) else { return }
-    SDWebImageManager.shared().loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
-      self.image1Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+    if let imageUrl = user?.imageUrl1, let url = URL(string: imageUrl) {
+      SDWebImageManager.shared().loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
+        self.image1Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+      }      
+    }
+    
+    if let imageUrl = user?.imageUrl2, let url = URL(string: imageUrl) {
+      SDWebImageManager.shared().loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
+        self.image2Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+      }
+    }
+    
+    if let imageUrl = user?.imageUrl3, let url = URL(string: imageUrl) {
+      SDWebImageManager.shared().loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
+        self.image3Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+      }
     }
   }
   
@@ -122,6 +134,8 @@ class SettingsController: UITableViewController {
       "uid": uid,
       "fullName": user?.name ?? "",
       "imageUrl1": user?.imageUrl1 ?? "",
+      "imageUrl2": user?.imageUrl2 ?? "",
+      "imageUrl3": user?.imageUrl3 ?? "",
       "age": user?.age ?? -1,
       "profession": user?.profession ?? ""
     ]
@@ -229,5 +243,41 @@ extension SettingsController: UIImagePickerControllerDelegate, UINavigationContr
     let imageButton = (picker as? CustomImagePickerController)?.imageButton
     imageButton?.setImage(selectedImage?.withRenderingMode(.alwaysOriginal), for: .normal)
     dismiss(animated: true)
+    
+    let fileName = UUID().uuidString
+    let ref = Storage.storage().reference(withPath: "/images/\(fileName)")
+    guard let uploadData = selectedImage?.jpegData(compressionQuality: 0.75) else { return }
+    
+    let hud = JGProgressHUD(style: .dark)
+    hud.textLabel.text = "Uploading image..."
+    hud.show(in: view)
+    
+    ref.putData(uploadData, metadata: nil) { (nil, err) in
+      if let err = err {
+        hud.dismiss()
+        print("Failed to upload image to storage", err)
+        return
+      }
+      
+      print("Finished uploading image")
+      ref.downloadURL { (url, err) in
+        hud.dismiss()
+        
+        if let err = err {
+          print("Failed to upload image to storage", err)
+          return
+        }
+        
+        print("Finished getting download url:", url?.absoluteString ?? "")
+        
+        if imageButton == self.image1Button {
+          self.user?.imageUrl1 = url?.absoluteString
+        } else if imageButton == self.image2Button {
+          self.user?.imageUrl2 = url?.absoluteString
+        } else {
+          self.user?.imageUrl3 = url?.absoluteString
+        }
+      }
+    }
   }
 }
