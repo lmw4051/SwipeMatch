b13df339e460b6c942907ed70eb8b83a20a43fce
David <dvdlee41@gmail.com>
2020-12-25 12:24:40 +0800

    . Adding CABasicAnimation for fixing bugs when click like/dislike button very fast . Fix bug after clicking save button, the like/dislike button no longer works

--- SwipeMatch/Controllers/HomeController.swift
+++ SwipeMatch/Controllers/HomeController.swift
@@ -32,6 +32,7 @@ class HomeController: UIViewController {
     topStackView.settingsButton.addTarget(self, action: #selector(handleSettings), for: .touchUpInside)
     bottomControls.refreshButton.addTarget(self, action: #selector(handleRefresh), for: .touchUpInside)
     bottomControls.likeButton.addTarget(self, action: #selector(handleLike), for: .touchUpInside)
+    bottomControls.dislikeButton.addTarget(self, action: #selector(handleDislike), for: .touchUpInside)
     
     setupLayout()
     fetchCurrentUser()
@@ -100,6 +101,9 @@ class HomeController: UIViewController {
     
     let query = Firestore.firestore().collection("users").whereField("age", isGreaterThanOrEqualTo: minAge).whereField("age", isLessThanOrEqualTo: maxAge)
     
+    // Fix bug after clicking save button, the like/dislike button no longer works
+    topCardView = nil
+    
     query.getDocuments { (snapshot, err) in
       self.hud.dismiss()
       
@@ -138,6 +142,32 @@ class HomeController: UIViewController {
     return cardView
   }
   
+  fileprivate func performSwipeAnimation(translation: CGFloat, angle: CGFloat) {
+    let duration = 0.5
+    let translationAnimation = CABasicAnimation(keyPath: "position.x")
+    translationAnimation.toValue = translation
+    translationAnimation.duration = duration
+    translationAnimation.fillMode = .forwards
+    translationAnimation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+    translationAnimation.isRemovedOnCompletion = false
+    
+    let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+    rotationAnimation.toValue = angle * CGFloat.pi / 180
+    rotationAnimation.duration = duration
+    
+    let cardView = topCardView
+    topCardView = cardView?.nextCardView
+    
+    CATransaction.setCompletionBlock {
+      cardView?.removeFromSuperview()
+    }
+    
+    cardView?.layer.add(translationAnimation, forKey: "translation")
+    cardView?.layer.add(rotationAnimation, forKey: "rotation")
+    
+    CATransaction.commit()
+  }
+  
   // MARK: - Selector Methods
   @objc func handleSettings() {
     let settingsController = SettingsController()
@@ -152,16 +182,11 @@ class HomeController: UIViewController {
   }
   
   @objc fileprivate func handleLike() {
-    print("handleLike")
-            
-    UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
-      self.topCardView?.frame = CGRect(x: 600, y: 0, width: self.topCardView!.frame.width, height: self.topCardView!.frame.height)
-      let angle = 15 * CGFloat.pi / 180
-      self.topCardView?.transform = CGAffineTransform(rotationAngle: angle)
-    }) { _ in
-      self.topCardView?.removeFromSuperview()
-      self.topCardView = self.topCardView?.nextCardView
-    }
+    performSwipeAnimation(translation: 700, angle: 15)
+  }
+  
+  @objc fileprivate func handleDislike() {
+    performSwipeAnimation(translation: -700, angle: -15)
   }
 }
 
